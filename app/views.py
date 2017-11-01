c37b34347f6c32643e817896f58f85dd110ce6bb
water <102245405@qq.com>
2017-11-01 11:02:35 +0800

    初始化库，添加框架代码

---
+++ app/views.py
@@ -0,0 +1,36 @@
+from flask import render_template,flash,redirect
+from app import app
+from .froms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname':'hell'}
+    posts = [
+    {
+        'author':{'nickname':'John'},
+        'body':'beautiful day!'
+    },
+    {
+        'author':{'nickname':'Susan'},
+        'body':'Flask is cool!'
+    }
+    ]
+    return render_template(
+        "index.html",
+        title = 'Home',
+        user = user,
+        posts = posts)
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + 
+        form.openid.data + '",remember_me=' 
+        + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+            title='Sign In',
+            form=form,
+            providers = app.config['OPENID_PROVIDERS'])
---
+++ config.py
@@ -0,0 +1,17 @@
+CSRF_ENABLE = True
+
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    { 'name': 'baidu', 'url': 'https://www.baidu.com' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
